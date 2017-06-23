bec79d86869462c2070b71f0ad2d58667b98f88d
Vincent <wenzhishen@icloud.com>
2017-06-23 16:11:41 +0800

    final

--- LabelTextChangeAnimation/LabelTextChangeAnimation/ViewController.m
+++ LabelTextChangeAnimation/LabelTextChangeAnimation/ViewController.m
@@ -9,9 +9,29 @@
 #import "ViewController.h"
 
 @interface ViewController ()
-
+@property (weak, nonatomic) IBOutlet UILabel *label;
+@property (nonatomic) NSInteger num;
 @end
 
 @implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    self.label.text = @"0";
+}
+
+- (IBAction)animate:(UIButton *)sender {
+    [self animateTextChange:0.2];
+    self.label.text = [NSString stringWithFormat:@"%zd", ++self.num % 10];
+}
+
+- (void)animateTextChange:(CFTimeInterval)duration {
+    CATransition *trans = [[CATransition alloc] init];
+    trans.type = kCATransitionPush;
+    trans.subtype = kCATransitionFromBottom;
+    trans.duration = duration;
+    trans.timingFunction = [CAMediaTimingFunction functionWithName:kCAMediaTimingFunctionEaseIn];
+    [self.label.layer addAnimation:trans forKey:kCATransitionPush];
+}
 
 @end
--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # LabelTextChangeAnimation
 UILabel 文字变化过渡动画：新文字往上滚动，将老文字挤走
+
+
+## 参照的 Swift 实现：
+
+[SwiftArchitect/SO-33632266](https://github.com/SwiftArchitect/SO-33632266)
+
+## 效果图
+
+![SwiftArchitect/SO-33632266](https://camo.githubusercontent.com/bbb7cb84723f67eb4d38a984b186c09135953db3/68747470733a2f2f692e737461636b2e696d6775722e636f6d2f74735530672e676966)
